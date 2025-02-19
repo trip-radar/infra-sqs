474eec0f41501a70324e823f84046cdb85970524
Lucas Lima <lucaslima_miml@hotmail.com>
2025-02-19 14:38:58 -0300

    chore(sqs): create sqs

--- infra/data.tf
+++ infra/data.tf
@@ -1,1 +1,19 @@
 data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "tripradar-sqs-policy" {
+  statement {
+    sid    = "tripradar-sqs-statement"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage"
+    ]
+    resources = [
+      aws_sqs_queue.tripradar-sqs-queue.arn
+    ]
+  }
+}
--- infra/main.tf
+++ infra/main.tf
@@ -1,31 +1,16 @@
-resource "aws_sqs_queue" "tripradar-sqs" {
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sqs_queue
+resource "aws_sqs_queue" "tripradar-sqs-queue" {
   name = local.queue_name
   delay_seconds = var.delay_seconds
-  message_retention_seconds = var.message_retention_seconds
-  receive_wait_time_seconds = var.receive_wait_time_seconds
   visibility_timeout_seconds = var.visibility_timeout_seconds
   max_message_size = var.max_message_size
-#  redrive_policy = jsonencode({
-#    deadLetterTargetArn = aws_sqs_queue.tripradar-sqs-dlq.arn
-#    maxReceiveCount = var.max_receive_count
-#  })
-#  kms_master_key_id = aws_kms_alias.tripradar-kms-key.arn
-#  kms_data_key_reuse_period_seconds = local.kms_data_key_reuse_period_seconds
-
+  message_retention_seconds = var.message_retention_seconds
+  receive_wait_time_seconds = var.receive_wait_time_seconds
+  sqs_managed_sse_enabled = true
 }
 
-#resource "aws_sqs_queue" "tripradar-sqs-dlq" {
-#  name = join("-", [local.queue_name, "dlq"])
-#  delay_seconds = var.delay_seconds_dlq
-#  message_retention_seconds = var.message_retention_seconds_dlq
-#  receive_wait_time_seconds = var.receive_wait_time_seconds_dlq
-#  visibility_timeout_seconds = var.visibility_timeout_seconds_dlq
-#  max_message_size = var.max_message_size
-#  redrive_policy = jsonencode({
-#    deadLetterTargetArn = aws_sqs_queue.tripradar-sqs-dlq.arn
-#    maxReceiveCount = var.max_receive_count
-#  })
-#  kms_master_key_id = aws_kms_alias.tripradar-kms-key.arn
-#  kms_data_key_reuse_period_seconds = local.kms_data_key_reuse_period_seconds
-#
-#}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sqs_queue_policy
+resource "aws_sqs_queue_policy" "tripradar-sqs-policy" {
+  queue_url = aws_sqs_queue.tripradar-sqs-queue.id
+  policy    = data.aws_iam_policy_document.tripradar-sqs-policy.json
+}
